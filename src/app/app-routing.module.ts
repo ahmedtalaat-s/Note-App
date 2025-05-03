5eabf8de4f437f11d3c7a939937fc9d71a19648d
marwaas3d <marwaas3d@gmail.com>
2025-05-03 05:32:41 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { BlankLayoutComponent } from './blank-layout/blank-layout.component';
+import { TableComponent } from './table/table.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'' , component: BlankLayoutComponent , children:[
+  {path:"" , redirectTo: 'table' , pathMatch: 'full'},
+  {path:"table" , component:TableComponent , title: "Your Tasks"},
+  {path:"addTask" , component: AddTaskComponent , title:"Add Task"}
+]}
+  ,
+  {path:'', component:AuthLayoutComponent , children:[
+    {path:"signIn" , component: SignInComponent , title:"Sign In"},
+    {path:"signUp" , component: SignUpComponent , title:"Sign Up"},
+  ]},
+  {path:"**" , component:NotfoundComponent , title:"Not Found"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TableComponent } from './table/table.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './blank-layout/blank-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    TableComponent,
+    AddTaskComponent,
+    NotfoundComponent,
+    AuthLayoutComponent,
+    BlankLayoutComponent
   ],
   imports: [
     BrowserModule,
